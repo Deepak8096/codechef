50cd2c8fd94b7b1c5fc883f7891ed81b0bf96f4a
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-06 11:28:21 +0530

    Add files via upload

---
+++ ice.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int Y = scanner.nextInt();
+        System.out.println(Math.min(X, Y));
+        scanner.close();
+    
+
+	}
+}

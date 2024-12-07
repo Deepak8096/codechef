fe514a7fbc3d5437e819593693a3baa38aa7e446
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-07 15:57:20 +0530

    Add files via upload

---
+++ codecf.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
+        if (N == 4) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }// your code goes here
+	}
+}

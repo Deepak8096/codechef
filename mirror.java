2c5e2a259445d628daf56b9551d524bb34d83686
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-07 15:51:14 +0530

    Add files via upload

---
+++ mirror.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println(N);
+        scanner.close();
+    }
+}

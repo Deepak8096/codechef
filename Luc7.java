565c1677fd1cbacd0122db2daedb92af0212164b
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-08 16:11:29 +0530

    Add files via upload

---
+++ Luc7.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
+        if (N == 7) {
+            System.out.println("THALA");
+        } else {
+            System.out.println("SADGE");
+        }
+        
+        scanner.close();// your code goes here
+
+	}
+}

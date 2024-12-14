eff7dc5e430b06844831b161974253d4caafbbae
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-14 15:46:36 +0530

    Add files via upload

---
+++ acpleace.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        
+        if (T > 30) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        
+        scanner.close();// your code goes here
+
+	}
+}

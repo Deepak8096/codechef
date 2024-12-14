43995e489ea44254f798d36848fb80a76a8fea61
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-14 15:12:51 +0530

    Add files via upload

---
+++ healthslp.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        int H = scanner.nextInt();  // Read the number of hours of sleep
+        
+        if (H < 8) {
+            System.out.println("LESS");
+        } else if (H == 8) {
+            System.out.println("PERFECT");
+        } else {
+            System.out.println("MORE");
+        }
+        
+        scanner.close();
+
+	}
+}

2cade6760c19e7cee8992b57729c26a4d9737c01
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-14 15:53:26 +0530

    Add files via upload

---
+++ doublerent.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        System.out.println(X * 2);// your code goes here
+
+	}
+}

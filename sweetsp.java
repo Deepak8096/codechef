b24d15f02a3033e57078359ff335b35a481cd41b
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-14 15:20:26 +0530

    Add files via upload

---
+++ sweetsp.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+        int remainingMoney = X - (N * 10);
+        int jalebis = remainingMoney / 20;
+        System.out.println(jalebis);
+
+	}
+}

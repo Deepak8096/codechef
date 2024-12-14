cb3d67f4dac66c075d5fd8505964d1be38d8d587
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-14 15:16:50 +0530

    Add files via upload

---
+++ chefcd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        
+        if (X >= 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        
+        scanner.close();
+    }
+}
